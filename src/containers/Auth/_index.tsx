c35d928400c4e40cf4676de218013afe2e83d0d4
Vilmar Cabañero <vilmarcabanero@gmail.com>
2022-08-07 04:52:31 +0800

    CB-37: Redirect to main page when user is logged in.

--- src/containers/Auth/_index.tsx
+++ src/containers/Auth/_index.tsx
@@ -1,9 +1,17 @@
-import * as React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
 import * as C from './components';
 import { AuthContainer } from './AuthContainer/_index';
 import { LeftPhotoContainer } from './LeftPhotoContainer/_index';
+import { useNavigate } from 'react-router-dom';
 
 export function Auth() {
+  const isLoggedIn = localStorage.getItem('authToken');
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isLoggedIn) navigate('/');
+  }, []);
+
   return (
     <C.Container>
       <LeftPhotoContainer />
